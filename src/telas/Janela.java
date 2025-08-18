960721845f2a6283889ac591b8cdc34d3b9dfb2c
maju2107 <maju.mariajuliaaraujo@gmail.com>
2025-08-18 09:52:45 -0300

    começo

---
+++ src/telas/Janela.java
@@ -0,0 +1,55 @@
+package telas;
+import javax.swing.*;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class Janela extends JFrame{
+
+    private JLabel jlAgencia;
+    private JTextField jtfAgencia;
+    private JLabel jlConta;
+    private JTextField jtfConta;
+    private JSeparator jSeparator01;
+    private JLabel jlNome;
+    private JTextField jtfNome;
+    private JLabel jlEndereco;
+    private JTextField jtfEndereco;
+    private JLabel jlTelefone;
+    private JTextField jtfTelefone;
+    private JLabel jlCpf;
+    private JTextField jtfCpf;
+    private JRadioButton jrbCorrente;
+    private JRadioButton jrbPoupanca;
+    private ButtonGroup bgContas;
+    private JSeparator jSeparator02;
+    private JButton jbConsultar;
+    private JButton jbAtualizar;
+    private JButton jbFechar;
+   
+
+    private void centralizar(){
+        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        Dimension janela = getSize();
+        if (janela.height > screen.height) 
+            setSize(janela.width, screen.height);
+        if (janela.width > screen.width)
+            setSize(screen.width, janela.height);
+        setLocation((screen.width - janela.width) / 2,
+                    (screen.height - janela.height) /2);
+       
+    }
+
+    public Janela(){
+        super("Java Swing - Desenvolvimento de Sistemas");
+        setSize(400,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        getContentPane().setLayout(null); // evitar que o usuário redimensione a janela
+
+        centralizar();
+
+    }
+
+
+    
+}
---
+++ src/telas/Main.java
@@ -0,0 +1,12 @@
+package telas;
+
+import javax.swing.JLabel;
+
+public class Main {
+    public static void main(String[] args) {
+        Janela janela = new Janela();
+        janela.setVisible(true);
+      
+    }
+
+} 
